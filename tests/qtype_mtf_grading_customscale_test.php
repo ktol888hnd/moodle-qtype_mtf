2c35bdb94b85853ed794ab63c2c12d1d70fb9f9b
ktol888hnd <31366429+ktol888hnd@users.noreply.github.com>
2026-09-03 01:28:06 +0700

    Add PHPUnit tests for customscale grading
    Add PHPUnit tests for customscale grading

--- tests/qtype_mtf_grading_customscale_test.php
+++ tests/qtype_mtf_grading_customscale_test.php
@@ -1,6 +1,8 @@
 <?php
 // This file is part of Moodle - http://moodle.org/.
 
+namespace qtype_mtf;
+
 defined('MOODLE_INTERNAL') || die();
 
 global $CFG;
@@ -15,145 +17,160 @@ require_once(
     '/question/type/mtf/question.php'
 );
 
+require_once(
+    $CFG->dirroot .
+    '/question/engine/tests/helpers.php'
+);
+
 /**
- * PHPUnit tests for customscale grading.
+ * Unit tests for customscale grading.
  *
  * @package     qtype_mtf
- * @covers      qtype_mtf_grading_customscale
+ * @group       qtype_mtf
  */
-final class qtype_mtf_grading_customscale_test extends advanced_testcase {
+final class qtype_mtf_grading_customscale_test extends \advanced_testcase {
 
     /**
-     * Build test question.
+     * Create a simple 4-row MTF question.
      *
-     * @return qtype_mtf_question
+     * @return \qtype_mtf_question
      */
-    protected function create_question(): qtype_mtf_question {
+    protected function get_question(): \qtype_mtf_question {
 
-        $question = new qtype_mtf_question();
+        \question_bank::load_question_definition_classes('mtf');
 
-        $question->numberofrows = 4;
-        $question->order = [1, 2, 3, 4];
+        $q = new \qtype_mtf_question();
 
-        $question->rows = [
+        $q->numberofrows = 4;
+        $q->numberofcolumns = 2;
+        $q->order = [1, 2, 3, 4];
+
+        $q->rows = [
             1 => (object)['number' => 1],
             2 => (object)['number' => 2],
             3 => (object)['number' => 3],
             4 => (object)['number' => 4],
         ];
 
-        $question->weights = [
-            1 => [1 => (object)['weight' => 1]],
-            2 => [1 => (object)['weight' => 1]],
-            3 => [1 => (object)['weight' => 1]],
-            4 => [1 => (object)['weight' => 1]],
+        $q->weights = [
+            1 => [
+                1 => (object)['weight' => 1.0],
+                2 => (object)['weight' => 0.0],
+            ],
+            2 => [
+                1 => (object)['weight' => 1.0],
+                2 => (object)['weight' => 0.0],
+            ],
+            3 => [
+                1 => (object)['weight' => 1.0],
+                2 => (object)['weight' => 0.0],
+            ],
+            4 => [
+                1 => (object)['weight' => 1.0],
+                2 => (object)['weight' => 0.0],
+            ],
         ];
 
-        return $question;
+        return $q;
     }
 
     /**
-     * 0 correct.
+     * @covers ::grade_question
      */
-    public function test_zero_correct(): void {
+    public function test_customscale_0_correct(): void {
 
-        $question = $this->create_question();
-        $grading = new qtype_mtf_grading_customscale();
+        $grading = new \qtype_mtf_grading_customscale();
 
-        $answers = [
-            'option0' => 2,
-            'option1' => 2,
-            'option2' => 2,
-            'option3' => 2,
-        ];
-
-        $this->assertEquals(
-            0.0,
-            $grading->grade_question($question, $answers)
+        $grade = $grading->grade_question(
+            $this->get_question(),
+            [
+                'option0' => 2,
+                'option1' => 2,
+                'option2' => 2,
+                'option3' => 2,
+            ]
         );
+
+        $this->assertEquals(0.0, $grade);
     }
 
     /**
-     * 1 correct.
+     * @covers ::grade_question
      */
-    public function test_one_correct(): void {
+    public function test_customscale_1_correct(): void {
 
-        $question = $this->create_question();
-        $grading = new qtype_mtf_grading_customscale();
+        $grading = new \qtype_mtf_grading_customscale();
 
-        $answers = [
-            'option0' => 1,
-            'option1' => 2,
-            'option2' => 2,
-            'option3' => 2,
-        ];
-
-        $this->assertEquals(
-            0.10,
-            $grading->grade_question($question, $answers)
+        $grade = $grading->grade_question(
+            $this->get_question(),
+            [
+                'option0' => 1,
+                'option1' => 2,
+                'option2' => 2,
+                'option3' => 2,
+            ]
         );
+
+        $this->assertEquals(0.10, $grade);
     }
 
     /**
-     * 2 correct.
+     * @covers ::grade_question
      */
-    public function test_two_correct(): void {
+    public function test_customscale_2_correct(): void {
 
-        $question = $this->create_question();
-        $grading = new qtype_mtf_grading_customscale();
+        $grading = new \qtype_mtf_grading_customscale();
 
-        $answers = [
-            'option0' => 1,
-            'option1' => 1,
-            'option2' => 2,
-            'option3' => 2,
-        ];
-
-        $this->assertEquals(
-            0.25,
-            $grading->grade_question($question, $answers)
+        $grade = $grading->grade_question(
+            $this->get_question(),
+            [
+                'option0' => 1,
+                'option1' => 1,
+                'option2' => 2,
+                'option3' => 2,
+            ]
         );
+
+        $this->assertEquals(0.25, $grade);
     }
 
     /**
-     * 3 correct.
+     * @covers ::grade_question
      */
-    public function test_three_correct(): void {
+    public function test_customscale_3_correct(): void {
 
-        $question = $this->create_question();
-        $grading = new qtype_mtf_grading_customscale();
+        $grading = new \qtype_mtf_grading_customscale();
 
-        $answers = [
-            'option0' => 1,
-            'option1' => 1,
-            'option2' => 1,
-            'option3' => 2,
-        ];
-
-        $this->assertEquals(
-            0.50,
-            $grading->grade_question($question, $answers)
+        $grade = $grading->grade_question(
+            $this->get_question(),
+            [
+                'option0' => 1,
+                'option1' => 1,
+                'option2' => 1,
+                'option3' => 2,
+            ]
         );
+
+        $this->assertEquals(0.50, $grade);
     }
 
     /**
-     * 4 correct.
+     * @covers ::grade_question
      */
-    public function test_four_correct(): void {
+    public function test_customscale_4_correct(): void {
 
-        $question = $this->create_question();
-        $grading = new qtype_mtf_grading_customscale();
+        $grading = new \qtype_mtf_grading_customscale();
 
-        $answers = [
-            'option0' => 1,
-            'option1' => 1,
-            'option2' => 1,
-            'option3' => 1,
-        ];
-
-        $this->assertEquals(
-            1.0,
-            $grading->grade_question($question, $answers)
+        $grade = $grading->grade_question(
+            $this->get_question(),
+            [
+                'option0' => 1,
+                'option1' => 1,
+                'option2' => 1,
+                'option3' => 1,
+            ]
         );
+
+        $this->assertEquals(1.0, $grade);
     }
 }
